b082adad205e4a10ecc58bacfd923f522fe32f63
Shameem <shameemakhtar111@gmail.com>
2025-03-24 23:31:58 +0530

    updated admin ui

--- polls/admin.py
+++ polls/admin.py
@@ -2,6 +2,28 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
-# Register your models here.
-admin.site.register(Question)
-admin.site.register(Choice)
+# class ChoiceInline(admin.StackedInline):    #takes a lot of space in the admin UI, that's why switch to TabularInline
+class ChoiceInline(admin.TabularInline):    
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ('Date Information', {"fields": ["pub_date"]}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ["question_text", "pub_date", "was_published_recently"]  # In the list of ques it will show tabular view having columns mention here
+    list_filter = ["pub_date"]  #Add filter option in admin page
+    search_fields = ["question_text"]
+    # fields = ["pub_date", "question_text"]    #can change the order of the column in db
+
+
+
+# version1 without ModelAdmin
+#admin.site.register(Question)
+
+#Version2 with ModelAdmin
+admin.site.register(Question, QuestionAdmin)
+
+# admin.site.register(Choice)
